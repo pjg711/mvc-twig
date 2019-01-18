3e570689f8af1991a1c8b7a6d66242e00010b02d
pjg711 <pablo.ju.garcia@gmail.com>
2019-01-18 01:30:45 -0300

    actualizando version phpunit de la 5 a la 7.5.2

---
+++ tests/Modelos/BaseDeDatosTest_.php
@@ -0,0 +1,16 @@
+<?php
+namespace MVCWeb\ModelosTest\Resumenes;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use MVCWeb\Modelos\Resumenes;
+include_once "src/config.inc.php";
+
+class BaseDeDatosTest extends TestCase
+{
+    use TestCaseTrait;
+    public function getConnection()
+    {
+        $pdo = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';charset=utf8', DB_SERVER_USERNAME, DB_SERVER_PASSWORD);
+        return $this->createDefaultDBConnection($pdo);
+    }
+}
